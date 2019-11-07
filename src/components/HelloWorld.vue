5b102780798c4a8e343fc96cc0ede4e87c17c58f
smartgmb0918 <vuehero18@outlook.com>
2019-11-07 04:15:29 -0800

    Add vuetify and some components

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <MyChip
+          :label="label"
+        />
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+import MyChip from './MyChip';
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    MyChip,
+  },
+  data() {
+    return {
+      selectedTitles: [
+        'Unreal',
+        'Untreal Tournament 2004',
+      ],
+      availableTitles: [
+        'Unreal',
+        'Unreal II',
+        'Unreal Tournament',
+        'Unreal Tournament 2003',
+        'Unreal Tournament 3',
+        'Untreal Tournament 2004',
+      ],
+      label: "Best UT Titles",
+    }
+  },
+  methods: {
+    ...mapActions('game', [
+      'setAvailable',
+    ]),
+    ...mapActions('game', {
+      setSelectedTitles: 'setSelected',
+    }),
+  },
+  mounted() {
+    this.setAvailable(this.availableTitles);
+    this.setSelectedTitles(this.selectedTitles);
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/MyChip.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-combobox
+      :value="getSelected"
+      :items="getAvailable"
+      :label="label"
+      chips
+      clearable
+      prepend-icon="filter_list"
+      solo
+      multiple
+      dark
+      @change="onChange"
+    >
+      <template slot="selection" slot-scope="data">
+        <v-chip
+          :selected="data.selected"
+          close
+          @input="remove(data.item)"
+        >
+          {{ data.item }}&nbsp;
+        </v-chip>
+      </template>
+    </v-combobox>
+
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="timeout"
+      color="red"
+      vertical
+    >
+      {{ text }}
+      <v-btn
+        dark
+        flat
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+    props: {
+      label: String,
+    },
+    computed: {
+      ...mapGetters('game', ['getSelected', 'getAvailable']),
+    },
+    data () {
+      return {
+        snackbar: false,
+        timeout: 6000,
+        text: 'Piece Removed'
+      }
+    },
+    methods: {
+      ...mapActions('game', ['setSelected']),
+      remove (item) {
+        const selected = Object.assign([], this.getSelected);
+        this.setSelected(selected.splice(selected.indexOf(item), 1));
+        this.snackbar = true;
+      },
+      onChange (titles) {
+        this.setSelected(titles);
+      }
+    },
+  };
+</script>
